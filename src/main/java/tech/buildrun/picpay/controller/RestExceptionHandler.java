c31ac4216434c15ba8feb0987ac24dcaaf2b6475
EliPeixoto <eliane.peixoto@outlook.com>
2024-08-16 21:25:31 -0300

    personalizando erro quando campo vazio

--- src/main/java/tech/buildrun/picpay/controller/RestExceptionHandler.java
+++ src/main/java/tech/buildrun/picpay/controller/RestExceptionHandler.java
@@ -1,6 +1,8 @@
 package tech.buildrun.picpay.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import tech.buildrun.picpay.exception.PicPayException;
@@ -11,4 +13,25 @@ public class RestExceptionHandler {
     public ProblemDetail handlePicPayException(PicPayException e){
         return e.toProblemDetail();
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ProblemDetail handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
+
+        var fieldErrors = e.getFieldErrors()
+                .stream()
+                .map(f -> new InvalidParam(f.getField(), f.getDefaultMessage()))
+                .toList();
+
+        var pb = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+        pb.setTitle("Your request parameters didn't validate");
+        pb.setProperty("invalid-params", fieldErrors);
+
+
+        return pb;
+    }
+    private record InvalidParam(String name, String reason){}
 }
+
+
+
+
